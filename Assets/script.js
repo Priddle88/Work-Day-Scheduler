7a90f1cd31aaded9c9e6c15352aaa4d88e14342d
Parker <prmac@Parkers-MacBook-Pro.local>
2022-07-24 18:43:25 -0600

    Added the time for one of the boxes

--- Assets/script.js
+++ Assets/script.js
@@ -2,3 +2,14 @@ var dateToday = moment().format("dddd, MMMM Do");
 
 $("#currentDay").text(dateToday);
 
+var time = document.querySelector(".hour").textContent;
+var description = document.querySelector(".description");
+
+console.log(time);
+var newTime = moment().hour(time).minute("0");
+
+console.log(newTime);
+
+if(newTime.isBefore()){
+    description.classList.add("past");
+}
