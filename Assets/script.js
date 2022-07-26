fc8139a0a6ec4765df9e36b93167dbfce315ec02
Parker <prmac@Parkers-MacBook-Pro.local>
2022-07-25 18:06:32 -0600

    Finally figured out what was wrong with the colors not working properly. It was the query selector not being set to All

--- Assets/script.js
+++ Assets/script.js
@@ -3,10 +3,14 @@ var dateToday = moment().format("dddd, MMMM Do");
 $("#currentDay").text(dateToday);
 
 var time = $(".hour").text();
-var inputs = $(".inputs");
 var timeArray = [];
 var newTime;
-var inputs = $("input");
+var inputs = document.querySelectorAll("input");
+var schedule = document.querySelector(".schedule");
+var userInput = document.querySelector("#what-to-do").value;
+var whatToDo = document.querySelector("#what-to-do").value;
+var btn = $(".saveBtn");
+var description = document.querySelectorAll(".description");
 
 
 console.log(time);
@@ -20,51 +24,57 @@ console.log(time);
 var newString = JSON.stringify(time);
 console.log(newString);
 var timeAm = newString.split(/AM|PM|"/);
-console.log(timeAm);
+var newArray = timeAm.slice(1,10);
+console.log(newArray);
 
-for (i = 5; i < timeAm.length - 1; i ++) {
-    timeAm[i] = Number(timeAm[i]) + 12;
+
+for (i = 4; i < newArray.length; i ++) {
+    newArray[i] = Number(newArray[i]) + 12;
 }
 
-var randTime = moment().set('hour', 14)
-console.log(randTime);
+// var randTime = moment().set('hour', 14);
+// console.log(randTime);
 // console.log(inputs);
+var rightNow = moment();
 
+console.log(rightNow);
 
-console.log(timeAm);
+console.log("yes" + newArray);
 
-// for (i = 0; i < timeAm.length ; i ++) {
-//     newTime = moment().set('hour', timeAm[i]);
+for (i = 0; i < newArray.length; i++) {
+    var newTime = moment().set('hour', newArray[i]);
+    
+    console.log(newTime);
+
+    if (newTime.isBefore(rightNow)){
+        inputs[i].classList.add("past");
+    } else if (newTime.isAfter(rightNow)) {
+            inputs[i].classList.add("future");
+         } else {
+            inputs[i].classList.add("present");
+         }
+}
+
+// for (i = 0; i < newArray.length; i++) {
+//     newTime = moment().set('hour', newArray[i]);
 //     console.log(newTime);
-//     if(newTime.isBefore()){
-//         inputs[i].classList.add("past");
-//     } else if (newTime.isAfter()) {
-//         inputs[i].classList.add("future");
-//     } else {
-//         inputs[i].classList.add("present");
-//     }
+//     if(newTime.isBefore(randTime, "hour")){
+//         description[i].classList.add("past");
+//      } else if (newTime.isAfter(randTime, "hour")) {
+//          description[i].classList.add("future");
+//      } else {
+//         description[i].classList.add("present");
+//      }
 // }
 
-for (i = 0; i < timeAm.length - 1; i ++) {
-    newTime = moment().set('hour', timeAm[i]);
-    console.log(newTime);
-    if(newTime.isSame(randTime, "hour")){
-        inputs[i].classList.add("present");
-    } else if (newTime.isAfter(randTime)) {
-        inputs[i].classList.add("future");
-    } else {
-        inputs[i].classList.add("past");
-    }
-}
+schedule.addEventListener("click", local); 
 
-// var newArray = timeArray.toString();
-//     console.log(newArray);
+function local(){
+    localStorage.setItem("UserInput", userInput);
+    console.log("I was run");
+};
 
-newTime = moment().set('hour', time[0]);
-
-
-
-console.log(newTime);
+// console.log(newTime);
 
 
 
