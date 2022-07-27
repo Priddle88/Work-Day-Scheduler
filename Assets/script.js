3175e47525a2fe6ea2da318ed9f21fe72b70c150
Parker <prmac@Parkers-MacBook-Pro.local>
2022-07-26 22:10:21 -0600

    Good for now. I am working on how to get the input history to erase

--- Assets/script.js
+++ Assets/script.js
@@ -7,19 +7,13 @@ var timeArray = [];
 var newTime;
 var inputs = document.querySelectorAll("input");
 var schedule = document.querySelector(".schedule");
-var userInput = document.querySelector("#what-to-do").value;
-var whatToDo = document.querySelector("#what-to-do").value;
-var btn = $(".saveBtn");
+var userInput = document.querySelector("#what-to-do");
+var whatToDo = document.querySelectorAll("#what-to-do").value;
+var btN = document.querySelectorAll(".saveBtn");
 var description = document.querySelectorAll(".description");
+var emptyA = [];
 
-
-console.log(time);
-
-// var timeAm = time.split(/AM|PM/);
-// timeArray.push(timeAm);
-// // var timepm = time.split("");
-// console.log(timeArray);
-// console.log(timeArray.length);
+var rightNow = moment();
 
 var newString = JSON.stringify(time);
 console.log(newString);
@@ -27,52 +21,90 @@ var timeAm = newString.split(/AM|PM|"/);
 var newArray = timeAm.slice(1,10);
 console.log(newArray);
 
-
-for (i = 4; i < newArray.length; i ++) {
-    newArray[i] = Number(newArray[i]) + 12;
-}
-
-// var randTime = moment().set('hour', 14);
-// console.log(randTime);
-// console.log(inputs);
-var rightNow = moment();
-
-console.log(rightNow);
-
-console.log("yes" + newArray);
-
-for (i = 0; i < newArray.length; i++) {
-    var newTime = moment().set('hour', newArray[i]);
+function start() {
+    for (i = 4; i < newArray.length; i ++) {
+        newArray[i] = Number(newArray[i]) + 12;
+    }
     
-    console.log(newTime);
-
-    if (newTime.isBefore(rightNow)){
-        inputs[i].classList.add("past");
-    } else if (newTime.isAfter(rightNow)) {
-            inputs[i].classList.add("future");
-         } else {
-            inputs[i].classList.add("present");
-         }
+    // var randTime = moment().set('hour', 14);
+    // console.log(randTime);
+    // console.log(inputs);
+    
+    
+    console.log(rightNow);
+    
+    console.log("yes" + newArray);
+    
+    for (i = 0; i < newArray.length; i++) {
+        var newTime = moment().set('hour', newArray[i]);
+        
+        console.log(newTime);
+    
+        if (newTime.isBefore(rightNow)){
+            inputs[i].classList.add("past");
+        } else if (newTime.isAfter(rightNow)) {
+                inputs[i].classList.add("future");
+             } else {
+                inputs[i].classList.add("present");
+             }
+    }
+    
+    // for (i = 0; i < newArray.length; i++) {
+    //     newTime = moment().set('hour', newArray[i]);
+    //     console.log(newTime);
+    //     if(newTime.isBefore(randTime, "hour")){
+    //         description[i].classList.add("past");
+    //      } else if (newTime.isAfter(randTime, "hour")) {
+    //          description[i].classList.add("future");
+    //      } else {
+    //         description[i].classList.add("present");
+    //      }
+    // }
 }
 
-// for (i = 0; i < newArray.length; i++) {
-//     newTime = moment().set('hour', newArray[i]);
-//     console.log(newTime);
-//     if(newTime.isBefore(randTime, "hour")){
-//         description[i].classList.add("past");
-//      } else if (newTime.isAfter(randTime, "hour")) {
-//          description[i].classList.add("future");
-//      } else {
-//         description[i].classList.add("present");
-//      }
-// }
 
-schedule.addEventListener("click", local); 
 
-function local(){
-    localStorage.setItem("UserInput", userInput);
-    console.log("I was run");
+
+console.log(JSON.stringify(userInput) + " UserInput");
+
+function storeInput(){
+    
+    
+    localStorage.setItem("UserInput", userInput.value());
+    console.log(`I was run ${userInput[0]}`);
 };
+
+function disItem() {
+    for (let index = 0; index < array.length; index++) {
+        
+        whatToDo = JSON.parse(localStorage.getItem("UserInput"));
+    }
+    
+}
+
+start();
+
+function btnEvent(event) {
+    event.preventDefault();
+    todoText = "";
+
+    
+
+
+        var todoText = userInput.value.trim();
+
+        emptyA.push(todoText);
+    
+        localStorage.setItem("UserInput", JSON.stringify(todoText));
+        userInput.textContent = JSON.parse(localStorage.getItem("UserInput"));
+        console.log(`I was run ${JSON.stringify(todoText)}`);
+    
+}
+
+for (let index = 0; index < btN.length; index++) {
+    btN[index].addEventListener("click", btnEvent);
+}
+
 
 // console.log(newTime);
 
